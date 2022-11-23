25bf9c85eda98d52a5688d778adadc550cce7bc7
Michael Tsitrin <michael@dymension.xyz>
2022-11-23 17:06:18 +0200

    cleanup

--- app/app.go
+++ app/app.go
@@ -742,7 +742,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(ibctransfertypes.ModuleName)
 	paramsKeeper.Subspace(ibchost.ModuleName)
 	paramsKeeper.Subspace(monitoringptypes.ModuleName)
-	// this line is used by starport scaffolding # stargate/app/paramSubspace
+	paramsKeeper.Subspace(wasm.ModuleName)
 
 	return paramsKeeper
 }
