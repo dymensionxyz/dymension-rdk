5750dd0af5439716352267f325440ff369cd0276
Michael Tsitrin <michael@dymension.xyz>
2022-12-11 20:32:36 +0200

    scaffold using v0.25.2

--- cmd/rollappd/main.go
+++ cmd/rollappd/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"os"
 
+	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
-	"github.com/dymensionxyz/rollapp/app"
-	"github.com/ignite/cli/ignite/pkg/cosmoscmd"
+
+	"rollapp/app"
+	"rollapp/cmd/rollappd/cmd"
 )
 
 func main() {
-	rootCmd, _ := cosmoscmd.NewRootCmd(
-		app.Name,
-		app.AccountAddressPrefix,
-		app.DefaultNodeHome,
-		app.Name,
-		app.ModuleBasics,
-		app.New,
-		// this line is used by starport scaffolding # root/arguments
-	)
-	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
-		os.Exit(1)
+	rootCmd, _ := cmd.NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
 	}
 }
--- readme.md
+++ readme.md
@@ -39,9 +39,9 @@ After a draft release is created, make your final changes from the release page 
 To install the latest version of your blockchain node's binary, execute the following command on your machine:
 
 ```
-curl https://get.ignite.com/dymensionxyz/rollapp@latest! | sudo bash
+curl https://get.ignite.com/username/rollapp@latest! | sudo bash
 ```
-`dymensionxyz/rollapp` should match the `username` and `repo_name` of the Github repository to which the source code was pushed. Learn more about [the install process](https://github.com/allinbits/starport-installer).
+`username/rollapp` should match the `username` and `repo_name` of the Github repository to which the source code was pushed. Learn more about [the install process](https://github.com/allinbits/starport-installer).
 
 ## Learn more
 
