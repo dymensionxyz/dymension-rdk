daaec67380957767b9716a02980a8f77dc2d7d53
Michael Tsitrin <michael@dymension.xyz>
2023-01-30 16:25:36 +0200

    make genesis tests compile

--- x/sequencers/genesis_test.go
+++ x/sequencers/genesis_test.go
@@ -5,24 +5,22 @@ import (
 
 	"github.com/dymensionxyz/rollapp/testutil/nullify"
 	"github.com/dymensionxyz/rollapp/x/sequencers"
+	"github.com/dymensionxyz/rollapp/x/sequencers/testutils"
 	"github.com/dymensionxyz/rollapp/x/sequencers/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGenesis(t *testing.T) {
+	k, ctx := testutils.NewTestSequencer(t)
+
 	genesisState := types.GenesisState{
 		Params: types.DefaultParams(),
-
-		// this line is used by starport scaffolding # genesis/test/state
 	}
 
-	k, ctx := keepertest.sequencersKeeper(t)
 	sequencers.InitGenesis(ctx, *k, genesisState)
 	got := sequencers.ExportGenesis(ctx, *k)
 	require.NotNil(t, got)
 
 	nullify.Fill(&genesisState)
 	nullify.Fill(got)
-
-	// this line is used by starport scaffolding # genesis/test/assert
 }
