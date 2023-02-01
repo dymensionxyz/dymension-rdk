66e74c4e93449834e405506426ee152032570a27
Michael Tsitrin <michael@dymension.xyz>
2023-02-01 13:42:54 +0200

    used the default Tendermint logger

--- cmd/rollappd/start.go
+++ cmd/rollappd/start.go
@@ -18,6 +18,7 @@ import (
 	"github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/proxy"
 	"google.golang.org/grpc"
+	"gopkg.in/natefinch/lumberjack.v2"
 
 	"github.com/cosmos/cosmos-sdk/server/api"
 	"github.com/cosmos/cosmos-sdk/server/config"
@@ -34,7 +35,6 @@ import (
 	dymintconv "github.com/dymensionxyz/dymint/conv"
 	dymintnode "github.com/dymensionxyz/dymint/node"
 	dymintrpc "github.com/dymensionxyz/dymint/rpc"
-	"github.com/dymensionxyz/rollapp/app"
 )
 
 const (
@@ -71,9 +71,10 @@ const (
 	flagGRPCAddress    = "grpc.address"
 	flagGRPCWebEnable  = "grpc-web.enable"
 	flagGRPCWebAddress = "grpc-web.address"
-)
 
-var log app.Logger
+	// logging flags
+	flagLogFile = "log-file"
+)
 
 // StartCmd runs the service passed in, either stand-alone or in-process with Dymint.
 func StartCmd(appCreator types.AppCreator, defaultNodeHome string) *cobra.Command {
@@ -120,14 +121,24 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			serverCtx := server.GetServerContextFromCmd(cmd)
 
-			home, err := cmd.Flags().GetString(flags.FlagHome)
-			if err != nil {
-				return err
-			}
+			log_path := serverCtx.Viper.GetString(flagLogFile)
+			fileInfo, err := os.Stat(log_path)
+			if err == nil && !fileInfo.IsDir() {
+				serverCtx.Logger.Info("Using rotating file for logging", "log_path", log_path)
+				logger := serverCtx.Logger.(server.ZeroLogWrapper).Output(&lumberjack.Logger{
+					Filename:   log_path,
+					MaxSize:    1000, // megabytes
+					MaxBackups: 3,
+					MaxAge:     28,   //days
+					Compress:   true, // disabled by default
+				})
 
-			//TODO: add flag to choose file logging or TTY
-			serverCtx.Logger = app.NewLogger(home)
-			//TODO: set log level
+				serverCtx.Logger = server.ZeroLogWrapper{
+					Logger: logger,
+				}
+			}
+			// We can use custom logger if needed
+			// serverCtx.Logger = app.NewLogger(home)
 
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
@@ -184,6 +195,8 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 	cmd.Flags().Uint32(FlagStateSyncSnapshotKeepRecent, 2, "State sync snapshot to keep")
 
 	cmd.Flags().Bool(FlagDisableIAVLFastNode, true, "Disable fast node for IAVL tree")
+
+	cmd.Flags().String(flagLogFile, "", "log file")
 
 	// add support for all Tendermint-specific command line options
 	tmcmd.AddNodeFlags(cmd)
