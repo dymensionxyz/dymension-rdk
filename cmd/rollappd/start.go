1c40dfd5ba464ac3df3cea291fb6a1bbb242e765
Omri <omritoptix@gmail.com>
2023-02-21 12:13:09 +0100

    feat: changed log rotate max size to be configurable vs hardcoded (#107)

--- cmd/rollappd/start.go
+++ cmd/rollappd/start.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"runtime/pprof"
+	"strconv"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -71,7 +72,8 @@ const (
 	flagGRPCWebAddress = "grpc-web.address"
 
 	// logging flags
-	flagLogFile = "log-file"
+	flagLogFile    = "log-file"
+	flagMaxLogSize = "max-log-size"
 )
 
 // StartCmd runs the service passed in, either stand-alone or in-process with Dymint.
@@ -118,14 +120,17 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 		},
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			serverCtx := server.GetServerContextFromCmd(cmd)
-
 			log_path := serverCtx.Viper.GetString(flagLogFile)
 			fileInfo, err := os.Stat(log_path)
 			if err == nil && !fileInfo.IsDir() {
 				serverCtx.Logger.Info("Using rotating file for logging", "log_path", log_path)
+				maxLogSize, err := strconv.Atoi(serverCtx.Viper.GetString(flagMaxLogSize))
+				if err != nil {
+					return err
+				}
 				logger := serverCtx.Logger.(server.ZeroLogWrapper).Output(&lumberjack.Logger{
 					Filename:   log_path,
-					MaxSize:    1000, // megabytes
+					MaxSize:    maxLogSize, // megabytes
 					MaxBackups: 3,
 					MaxAge:     28,   //days
 					Compress:   true, // disabled by default
@@ -195,6 +200,7 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 	cmd.Flags().Bool(FlagDisableIAVLFastNode, true, "Disable fast node for IAVL tree")
 
 	cmd.Flags().String(flagLogFile, "", "log file")
+	cmd.Flags().String(flagMaxLogSize, "1000", "Max log size in MB")
 
 	// add support for all Tendermint-specific command line options
 	tmcmd.AddNodeFlags(cmd)
