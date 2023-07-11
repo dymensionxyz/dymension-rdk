4d0e9cb2f122c5dd789f0beb175ad729a012d56d
Michael Tsitrin <michael@dymension.xyz>
2023-07-11 11:42:42 +0300

    exported add flags command

--- utils/logger/flags.go
+++ utils/logger/flags.go
@@ -10,7 +10,7 @@ const (
 	FlagModuleLogLevelOverride = "module-log-level-override"
 )
 
-func addLogFlags(cmd *cobra.Command) {
+func AddLogFlags(cmd *cobra.Command) {
 	cmd.Flags().String(FlagLogLevel, "debug", "Log leve. one of [\"debug\", \"info\", \"warn\", \"error\", \"fatal\"]")
 	cmd.Flags().String(FlagLogFile, "", "log file full path. If not set, logs to stdout")
 	cmd.Flags().String(FlagMaxLogSize, "1000", "Max log size in MB")
