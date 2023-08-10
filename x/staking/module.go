293b195a7da481540f67611800eb7057e244ca74
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2023-08-10 15:00:36 +0300

    feat: support stakers on genesis (#260)

--- x/staking/module.go
+++ x/staking/module.go
@@ -1,6 +1,8 @@
 package staking
 
 import (
+	"encoding/json"
+
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -9,8 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
-
-	"github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	"github.com/dymensionxyz/dymension-rdk/x/staking/keeper"
 )
 
 var (
@@ -31,12 +32,19 @@ type AppModule struct {
 // NewAppModule creates a new AppModule object
 func NewAppModule(cdc codec.Codec, keeper keeper.Keeper, ak types.AccountKeeper, bk types.BankKeeper) AppModule {
 	return AppModule{
-		AppModule: staking.NewAppModule(cdc, keeper, ak, bk),
+		AppModule: staking.NewAppModule(cdc, keeper.Keeper, ak, bk),
 		keeper:    keeper,
 	}
 }
 
 func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	_ = staking.EndBlocker(ctx, am.keeper)
+	_ = staking.EndBlocker(ctx, am.keeper.Keeper)
+	return []abci.ValidatorUpdate{}
+}
+
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
+	var genesisState types.GenesisState
+	cdc.MustUnmarshalJSON(data, &genesisState)
+	_ = am.keeper.InitGenesis(ctx, &genesisState)
 	return []abci.ValidatorUpdate{}
 }
