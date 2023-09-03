18e04afbe4619f23bf3bfc0614a7f3f42f5ac1ea
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2023-09-03 10:31:49 +0300

    chore: renaming Agents to Members (#267)

--- x/dist/doc.go
+++ x/dist/doc.go
@@ -1,5 +1,5 @@
 /*
-Package staking defines a "wrapper" module around the Cosmos SDK's native
+Package dist defines a "wrapper" module around the Cosmos SDK's native
 x/distribution module. In other words, it provides the exact same functionality as
 the native module in that it simply embeds the native module. However, it
 overrides the AllocateTokens function so the the allocation of rewards is done
