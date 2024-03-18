967d6520913fede40a0eb54a62e38000b227b383
hoangdv2429 <hoangdv2429@gmail.com>
2024-03-18 12:28:50 +0700

    optimize and msg server test

--- testutil/keepers/keepers.go
+++ testutil/keepers/keepers.go
@@ -1,18 +1,23 @@
 package keepers
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	epochkeeper "github.com/dymensionxyz/dymension-rdk/x/epochs/keeper"
 	mintkeeper "github.com/dymensionxyz/dymension-rdk/x/mint/keeper"
 	seqkeeper "github.com/dymensionxyz/dymension-rdk/x/sequencers/keeper"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	app "github.com/dymensionxyz/dymension-rdk/testutil/app"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmdb "github.com/tendermint/tm-db"
 )
 
 func NewTestEpochKeeperFromApp(t *testing.T, app *app.App) (*epochkeeper.Keeper, sdk.Context) {
@@ -37,6 +42,15 @@ func NewTestMintKeeperFromApp(t *testing.T, app *app.App) (*mintkeeper.Keeper, s
 // TODO: when dymension release denommetadata module, replace this with denommetadata keeper and remove this
 func NewTestBankKeeperFromApp(t *testing.T, app *app.App) (*bankkeeper.Keeper, sdk.Context) {
 	k := &app.BankKeeper
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{Height: 1, ChainID: "rollapp-1", Time: time.Now().UTC()})
+	bankStoreKey := storetypes.NewKVStoreKey(banktypes.StoreKey)
+	bank_t_storeKey := storetypes.NewTransientStoreKey("t_" + banktypes.StoreKey)
+	db := tmdb.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db)
+	stateStore.MountStoreWithDB(bankStoreKey, storetypes.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(bank_t_storeKey, storetypes.StoreTypeTransient, nil)
+	if err := stateStore.LoadLatestVersion(); err != nil {
+		panic(fmt.Errorf("failed to load latest version: %w", err))
+	}
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{Height: 1, ChainID: "rollapp-1", Time: time.Now().UTC()}).WithMultiStore(stateStore)
 	return k, ctx
 }
