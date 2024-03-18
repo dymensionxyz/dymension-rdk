2d79ce37f1855ec4a46f2d606c46def55b56dd3d
Michael Tsitrin <michael@dymension.xyz>
2024-03-18 10:27:27 +0200

    fixed linter

--- x/dist/keeper/allocation.go
+++ x/dist/keeper/allocation.go
@@ -28,13 +28,11 @@ func (k Keeper) AllocateTokens(ctx sdk.Context, blockProposer sdk.ConsAddress) {
 
 	/* ---------------------------- Pay the proposer ---------------------------- */
 	// calculate and pay proposer reward
-	proposerReward := sdk.DecCoins{}
 	proposerValidator, found := k.seqKeeper.GetSequencerByConsAddr(ctx, blockProposer)
 	if !found {
 		logger.Error("failed to find the validator for this block. reward not allocated")
 	} else {
-		proposerReward = feesCollected.MulDecTruncate(k.GetBaseProposerReward(ctx))
-
+		proposerReward := feesCollected.MulDecTruncate(k.GetBaseProposerReward(ctx))
 		proposerCoins, proposerRemainder := proposerReward.TruncateDecimal()
 		if !proposerCoins.IsZero() {
 			err := k.AllocateTokensToSequencer(ctx, proposerValidator, proposerCoins)
