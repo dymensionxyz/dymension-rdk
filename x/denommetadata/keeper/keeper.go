4286ec7529b2073c71653375f3a630652a2eae6c
hoangdv2429 <hoangdv2429@gmail.com>
2024-03-19 19:54:23 +0700

    name change and remove unnecessary

--- x/denommetadata/keeper/keeper.go
+++ x/denommetadata/keeper/keeper.go
@@ -41,13 +41,13 @@ func NewKeeper(
 	}
 }
 
-// GetParams returns the total set of minting parameters.
+// GetParams returns the total set of denommetadata parameters.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 	k.paramSpace.GetParamSet(ctx, &params)
 	return params
 }
 
-// SetParams sets the total set of minting parameters.
+// SetParams sets the total set of denommetadata parameters.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramSpace.SetParamSet(ctx, &params)
 }
