13cd0ce2d2ebd36b67de285d1c1bf0b11d3bf4ba
Michael Tsitrin <michael@dymension.xyz>
2024-03-25 22:47:34 +0200

    validating genesis

--- x/sequencers/types/genesis_test.go
+++ x/sequencers/types/genesis_test.go
@@ -3,22 +3,42 @@ package types_test
 import (
 	"testing"
 
+	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/dymensionxyz/dymension-rdk/x/sequencers/types"
 	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func TestGenesisState_Validate(t *testing.T) {
+	pk := ed25519.GenPrivKey().PubKey()
+
 	for _, tc := range []struct {
 		desc     string
 		genState types.GenesisState
 		valid    bool
 	}{
 		{
-			desc:     "default",
-			genState: *types.DefaultGenesis(),
-			valid:    true,
+			desc: "valid",
+			genState: types.GenesisState{
+				Params:                 types.DefaultParams(),
+				GenesisOperatorAddress: sdk.ValAddress(pk.Address()).String(),
+			},
+			valid: true,
 		},
-		//TODO: bad params
+		{
+			desc:     "default - missing operator address",
+			genState: *types.DefaultGenesis(),
+			valid:    false,
+		},
+		{
+			desc: "not a val address",
+			genState: types.GenesisState{
+				Params:                 types.DefaultParams(),
+				GenesisOperatorAddress: sdk.AccAddress(pk.Address()).String(),
+			},
+			valid: false,
+		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
 			err := tc.genState.ValidateGenesis()
