836c447321ceac04cdc298447de2ff9b1e4ff384
Michael Tsitrin <michael@dymension.xyz>
2024-04-08 10:15:31 +0300

    moved genesis under keeper package

---
+++ x/sequencers/genesis_test.go
@@ -0,0 +1,24 @@
+package sequencers_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/dymensionxyz/dymension-rdk/testutil/nullify"
+	"github.com/dymensionxyz/dymension-rdk/x/sequencers/types"
+)
+
+// TODO: add specific test and scenario
+func TestEpochsInitAndExportGenesis(t *testing.T) {
+	genesisState := types.GenesisState{}
+	ctx, epochsKeeper := Setup(t)
+
+	epochsKeeper.InitGenesis(ctx, genesisState)
+	genesis := epochsKeeper.ExportGenesis(ctx)
+	require.NotNil(t, genesis)
+	require.Len(t, genesis.Epochs, 5)
+	nullify.Fill(&genesis)
+	nullify.Fill(genesis)
+
+}
--- x/sequencers/keeper/genesis.go
+++ x/sequencers/keeper/genesis.go
@@ -1,16 +1,15 @@
-package sequencers
+package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 
-	"github.com/dymensionxyz/dymension-rdk/x/sequencers/keeper"
 	"github.com/dymensionxyz/dymension-rdk/x/sequencers/types"
 )
 
-// InitGenesis initializes the capability module's state from a provided genesis state.
+// InitGenesis initializes the sequencers module's state from a provided genesis state.
 // We return the for ValidatorUpdate only the sequencers set by dymint
-func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) []abci.ValidatorUpdate {
+func (k *Keeper) InitGenesis(ctx sdk.Context, genState types.GenesisState) []abci.ValidatorUpdate {
 	var updates []abci.ValidatorUpdate
 	k.SetParams(ctx, genState.Params)
 
@@ -56,9 +55,10 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) 
 	return updates
 }
 
-// ExportGenesis returns the capability module's exported genesis.
-func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+// ExportGenesis returns the sequencers module's exported genesis.
+func (k *Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 	genesis.Params = k.GetParams(ctx)
+
 	return genesis
 }
