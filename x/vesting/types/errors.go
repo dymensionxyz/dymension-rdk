bd98a96179374dcc3ab7b71435d82a6acbfe9c2c
Trinity <trinity6569@gmail.com>
2024-04-23 13:33:59 +0700

    lint

--- x/vesting/types/errors.go
+++ x/vesting/types/errors.go
@@ -6,6 +6,6 @@ import (
 
 // errors
 var (
-	ErrDuplicate          = errorsmod.Register(ModuleName, 301, "duplicate")
-	ErrNoPermission       = errorsmod.Register(ModuleName, 302, "signer not in the permissions list to create vesting account")
+	ErrDuplicate    = errorsmod.Register(ModuleName, 301, "duplicate")
+	ErrNoPermission = errorsmod.Register(ModuleName, 302, "signer not in the permissions list to create vesting account")
 )
