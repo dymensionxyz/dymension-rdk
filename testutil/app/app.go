e81ade4bd1fbeaf344c6dda46a7f81a01448719c
Omri <omritoptix@gmail.com>
2024-05-05 15:37:41 +0200

    Update ibcKeeper use SequencersKeeper instead of stakingKeeper

--- testutil/app/app.go
+++ testutil/app/app.go
@@ -366,7 +366,7 @@ func NewRollapp(
 	)
 
 	app.IBCKeeper = ibckeeper.NewKeeper(
-		appCodec, keys[ibchost.StoreKey], app.GetSubspace(ibchost.ModuleName), app.StakingKeeper, app.UpgradeKeeper, scopedIBCKeeper,
+		appCodec, keys[ibchost.StoreKey], app.GetSubspace(ibchost.ModuleName), app.SequencersKeeper, app.UpgradeKeeper, scopedIBCKeeper,
 	)
 
 	// Register the proposal types
