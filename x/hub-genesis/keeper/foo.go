81980d9895b7739be050b1c13368dfa7fc54f76d
danwt <30197399+danwt@users.noreply.github.com>
2024-05-24 14:31:30 +0100

    adds foo without breaking things

--- x/hub-genesis/keeper/foo.go
+++ x/hub-genesis/keeper/foo.go
@@ -8,17 +8,18 @@ import (
 	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
 	porttypes "github.com/cosmos/ibc-go/v6/modules/core/05-port/types"
 	"github.com/dymensionxyz/dymension-rdk/x/hub-genesis/types"
-	transferkeeper "github.com/evmos/evmos/v12/x/ibc/transfer/keeper"
 )
 
 type OnChanOpenConfirmInterceptor struct {
 	porttypes.IBCModule
-	transferK transferkeeper.Keeper
-	k         Keeper
+	transfer Transfer
+	k        Keeper
 }
 
-func NewOnChanOpenConfirmInterceptor(next porttypes.IBCModule, transferK transferkeeper.Keeper, k Keeper) *OnChanOpenConfirmInterceptor {
-	return &OnChanOpenConfirmInterceptor{next, transferK, k}
+type Transfer func(ctx sdk.Context, transfer *transfertypes.MsgTransfer) error
+
+func NewOnChanOpenConfirmInterceptor(next porttypes.IBCModule, t Transfer, k Keeper) *OnChanOpenConfirmInterceptor {
+	return &OnChanOpenConfirmInterceptor{next, t, k}
 }
 
 func (i OnChanOpenConfirmInterceptor) OnChanOpenConfirm(
@@ -56,7 +57,7 @@ func (i OnChanOpenConfirmInterceptor) OnChanOpenConfirm(
 		Memo:             "special",
 	}
 
-	_, err = i.transferK.Transfer(sdk.WrapSDKContext(ctx), &m)
+	err = i.transfer(ctx, &m)
 	if err != nil {
 		ctx.Logger().Error("OnChanOpenConfirm transfer", "err", err)
 	} else {
