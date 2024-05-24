a0d0015504be55dc3e6594f0029301c7ed2fc321
danwt <30197399+danwt@users.noreply.github.com>
2024-05-24 16:46:10 +0100

    fix old genesis event validation

--- x/hub-genesis/types/message_hub_genesis_event.go
+++ x/hub-genesis/types/message_hub_genesis_event.go
@@ -1,9 +1,7 @@
 package types
 
 import (
-	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 const TypeMsgHubGenesisEvent = "hub_genesis_event"
@@ -11,11 +9,7 @@ const TypeMsgHubGenesisEvent = "hub_genesis_event"
 var _ sdk.Msg = &MsgHubGenesisEvent{}
 
 func NewMsgHubGenesisEvent(address, channelId, hubId string) *MsgHubGenesisEvent {
-	return &MsgHubGenesisEvent{
-		Address:   address,
-		ChannelId: channelId,
-		HubId:     hubId,
-	}
+	return &MsgHubGenesisEvent{}
 }
 
 func (msg *MsgHubGenesisEvent) Route() string {
@@ -27,31 +21,13 @@ func (msg *MsgHubGenesisEvent) Type() string {
 }
 
 func (msg *MsgHubGenesisEvent) GetSigners() []sdk.AccAddress {
-	address, err := sdk.AccAddressFromBech32(msg.Address)
-	if err != nil {
-		panic(err)
-	}
-	return []sdk.AccAddress{address}
+	return nil
 }
 
 func (msg *MsgHubGenesisEvent) GetSignBytes() []byte {
-	bz := moduleCdc.MustMarshalJSON(msg)
-	return sdk.MustSortJSON(bz)
+	return nil
 }
 
 func (msg *MsgHubGenesisEvent) ValidateBasic() error {
-	_, err := sdk.AccAddressFromBech32(msg.Address)
-	if err != nil {
-		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid address (%s)", err)
-	}
-
-	if msg.ChannelId == "" {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "channel id cannot be empty")
-	}
-
-	if msg.HubId == "" {
-		return errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "hub id cannot be empty")
-	}
-
 	return nil
 }
