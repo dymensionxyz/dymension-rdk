2c514ae02ef0ce32bb279fd9fa1aec7624610f0c
danwt <30197399+danwt@users.noreply.github.com>
2024-05-24 11:20:50 +0100

    lets try without codec

--- x/hubgenesis/types/params.pb.go
+++ x/hubgenesis/types/params.pb.go
@@ -5,20 +5,17 @@ package types
 
 import (
 	fmt "fmt"
+	_ "github.com/cosmos/gogoproto/gogoproto"
+	proto "github.com/cosmos/gogoproto/proto"
 	io "io"
 	math "math"
 	math_bits "math/bits"
-
-	_ "github.com/cosmos/gogoproto/gogoproto"
-	proto "github.com/cosmos/gogoproto/proto"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
-var (
-	_ = proto.Marshal
-	_ = fmt.Errorf
-	_ = math.Inf
-)
+var _ = proto.Marshal
+var _ = fmt.Errorf
+var _ = math.Inf
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the proto package it is being compiled against.
@@ -39,11 +36,9 @@ func (*Params) ProtoMessage() {}
 func (*Params) Descriptor() ([]byte, []int) {
 	return fileDescriptor_7557419be61afd0c, []int{0}
 }
-
 func (m *Params) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-
 func (m *Params) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
 		return xxx_messageInfo_Params.Marshal(b, m, deterministic)
@@ -56,15 +51,12 @@ func (m *Params) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 		return b[:n], nil
 	}
 }
-
 func (m *Params) XXX_Merge(src proto.Message) {
 	xxx_messageInfo_Params.Merge(m, src)
 }
-
 func (m *Params) XXX_Size() int {
 	return m.Size()
 }
-
 func (m *Params) XXX_DiscardUnknown() {
 	xxx_messageInfo_Params.DiscardUnknown(m)
 }
@@ -90,11 +82,9 @@ func (*GenesisTriggererParams) ProtoMessage()    {}
 func (*GenesisTriggererParams) Descriptor() ([]byte, []int) {
 	return fileDescriptor_7557419be61afd0c, []int{1}
 }
-
 func (m *GenesisTriggererParams) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
 }
-
 func (m *GenesisTriggererParams) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	if deterministic {
 		return xxx_messageInfo_GenesisTriggererParams.Marshal(b, m, deterministic)
@@ -107,15 +97,12 @@ func (m *GenesisTriggererParams) XXX_Marshal(b []byte, deterministic bool) ([]by
 		return b[:n], nil
 	}
 }
-
 func (m *GenesisTriggererParams) XXX_Merge(src proto.Message) {
 	xxx_messageInfo_GenesisTriggererParams.Merge(m, src)
 }
-
 func (m *GenesisTriggererParams) XXX_Size() int {
 	return m.Size()
 }
-
 func (m *GenesisTriggererParams) XXX_DiscardUnknown() {
 	xxx_messageInfo_GenesisTriggererParams.DiscardUnknown(m)
 }
@@ -238,7 +225,6 @@ func encodeVarintParams(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
-
 func (m *Params) Size() (n int) {
 	if m == nil {
 		return 0
@@ -270,11 +256,9 @@ func (m *GenesisTriggererParams) Size() (n int) {
 func sovParams(x uint64) (n int) {
 	return (math_bits.Len64(x|1) + 6) / 7
 }
-
 func sozParams(x uint64) (n int) {
 	return sovParams(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
-
 func (m *Params) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
@@ -359,7 +343,6 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-
 func (m *GenesisTriggererParams) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
@@ -442,7 +425,6 @@ func (m *GenesisTriggererParams) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
-
 func skipParams(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
