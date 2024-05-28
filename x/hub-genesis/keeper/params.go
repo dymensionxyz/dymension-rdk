c20b172f57e3371f2a9c2ec47aa27bd3170d6249
danwt <30197399+danwt@users.noreply.github.com>
2024-05-28 15:40:20 +0100

    remove more unused residue of the genesis allow list

--- x/hub-genesis/keeper/params.go
+++ x/hub-genesis/keeper/params.go
@@ -15,19 +15,3 @@ func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
 }
-
-// GenesisTriggererAllowlist returns the GenesisTriggererAllowlist param
-func (k Keeper) GenesisTriggererAllowlist(ctx sdk.Context) (res []types.GenesisTriggererParams) {
-	k.paramstore.Get(ctx, types.KeyGenesisTriggererAllowlist, &res)
-	return
-}
-
-func (k Keeper) IsAddressInGenesisTriggererAllowList(ctx sdk.Context, address string) bool {
-	Allowlist := k.GenesisTriggererAllowlist(ctx)
-	for _, item := range Allowlist {
-		if item.Address == address {
-			return true
-		}
-	}
-	return false
-}
