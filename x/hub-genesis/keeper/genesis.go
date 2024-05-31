3f9cfacf27da322cd913a7160fdde0f7823169d3
danwt <30197399+danwt@users.noreply.github.com>
2024-05-31 16:00:57 +0100

    cp

--- x/hub-genesis/keeper/genesis.go
+++ x/hub-genesis/keeper/genesis.go
@@ -12,6 +12,8 @@ func (k Keeper) InitGenesis(ctx sdk.Context, genState *types.GenesisState) {
 	/*
 		Mint coins which will later be transferred to the hub
 		TODO: need to not do it if it not height 0 genesis
+
+		TODO: move to foo
 	*/
 	for _, ga := range genState.State.GetGenesisAccounts() {
 		coin := ga.GetAmount()
