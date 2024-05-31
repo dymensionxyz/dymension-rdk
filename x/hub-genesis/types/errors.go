f9b7b8fa85664049ef0b2e22fd5cf6aacdb50b09
danwt <30197399+danwt@users.noreply.github.com>
2024-05-31 14:35:52 +0100

    trying to clear up erorrs

--- x/hub-genesis/types/errors.go
+++ x/hub-genesis/types/errors.go
@@ -5,9 +5,7 @@ import errorsmod "cosmossdk.io/errors"
 var (
 	ErrFailedGetClientState         = errorsmod.Register(ModuleName, 1001, "failed to get client state")
 	ErrChainIDMismatch              = errorsmod.Register(ModuleName, 1002, "chain ID not matches with the channel")
-	ErrInvalidGenesisTokens         = errorsmod.Register(ModuleName, 1003, "invalid genesis token")
+	ErrInvalidGenesisTokens         = errorsmod.Register(ModuleName, 1003, "invalid genesis token") // TODO: use it(?) where was it originally used?
 	ErrGenesisEventAlreadyTriggered = errorsmod.Register(ModuleName, 1004, "genesis event already triggered")
-	ErrGenesisInsufficientBalance   = errorsmod.Register(ModuleName, 1005, "insufficient balance in module account to lock genesis tokens")
-	ErrLockingGenesisTokens         = errorsmod.Register(ModuleName, 1006, "failed to lock genesis tokens")
-	ErrWrongGenesisBalance          = errorsmod.Register(ModuleName, 1007, "genesis bank balance different than expected genesis tokens")
+	ErrLockingGenesisTokens         = errorsmod.Register(ModuleName, 1005, "failed to lock genesis tokens")
 )
