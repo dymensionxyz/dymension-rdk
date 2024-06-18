9f0d5030950cc2d870d130fd57d1a18c48ec9d37
danwt <30197399+danwt@users.noreply.github.com>
2024-06-18 09:42:10 +0100

    implements state validation

--- x/hub-genesis/types/state.go
+++ x/hub-genesis/types/state.go
@@ -1,6 +1,19 @@
 package types
 
+import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 func (s State) Validate() error {
-	// TODO: implement
+	for _, a := range s.GetGenesisAccounts() {
+		if err := a.GetAmount().Validate(); err != nil {
+			return errorsmod.Wrap(err, "amount")
+		}
+		_, err := sdk.AccAddressFromBech32(a.GetAddress())
+		if err != nil {
+			return errorsmod.Wrap(err, "address from bech 32")
+		}
+	}
 	return nil
 }
