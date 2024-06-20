6612b8d955c4bcf948e71259a83d320d2ab0bbf3
danwt <30197399+danwt@users.noreply.github.com>
2024-06-20 16:39:14 +0100

    just realised you cant use a cnter

--- x/hub-genesis/keeper/sequence_numbers.go
+++ x/hub-genesis/keeper/sequence_numbers.go
@@ -1,7 +1,6 @@
 package keeper
 
 import (
-	"encoding/binary"
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,20 +11,40 @@ Whenever a genesis transfer is sent, we record the sequence number. We do not al
 all acks have been received with success.
 */
 
-func seqNumKey(port, channel string) []byte {
-	return []byte(fmt.Sprintf("seqnum/%s/%s", port, channel))
+func seqNumKey(port, channel string, seq uint64) []byte {
+	bz := []byte(fmt.Sprintf("seqnumval/%s/%s", port, channel))
+	bz = append(bz, sdk.Uint64ToBigEndian(seq)...)
+	return bz
+}
+
+func numUnackedSeqNumsKey(port, channel string) []byte {
+	bz := []byte(fmt.Sprintf("seqnumcnt/%s/%s", port, channel))
+	return bz
 }
 
 func (k Keeper) saveSeqNum(ctx sdk.Context, port, channel string, seq uint64) {
-	seqBz := make([]byte, 8)
-	binary.BigEndian.PutUint64(seqBz, seq)
-	ctx.KVStore(k.storeKey).Set(seqNumKey(port, channel), seqBz)
+	ctx.KVStore(k.storeKey).Set(seqNumKey(port, channel, seq), []byte{})
+	cnt := k.getNumUnackedSeqNums(ctx, port, channel)
+	cnt++
+	k.saveNumUnackedSeqNums(ctx, port, channel, cnt)
 }
 
 func (k Keeper) delSeqNum(ctx sdk.Context, port, channel string, seq uint64) {
-	seqBz := make([]byte, 8)
-	binary.BigEndian.PutUint64(seqBz, seq)
-	ctx.KVStore(k.storeKey).Set(seqNumKey(port, channel), seqBz)
+	ctx.KVStore(k.storeKey).Delete(seqNumKey(port, channel, seq))
+}
+
+func (k Keeper) hasSeqNum(ctx sdk.Context, port, channel string, seq uint64) bool {
+	return ctx.KVStore(k.storeKey).Has(seqNumKey(port, channel, seq))
+}
+
+func (k Keeper) saveNumUnackedSeqNums(ctx sdk.Context, port, channel string, cnt uint64) {
+	bz := sdk.Uint64ToBigEndian(cnt)
+	ctx.KVStore(k.storeKey).Set(numUnackedSeqNumsKey(port, channel), bz)
+}
+
+func (k Keeper) getNumUnackedSeqNums(ctx sdk.Context, port, channel string) uint64 {
+	bz := ctx.KVStore(k.storeKey).Get(numUnackedSeqNumsKey(port, channel))
+	return sdk.BigEndianToUint64(bz)
 }
 
 // ackSeqNum handles the inbound acknowledgement of an outbound genesis transfer
@@ -33,11 +52,18 @@ func (k Keeper) ackSeqNum(ctx sdk.Context, port, channel string, seq uint64, suc
 	if !success {
 		panic(fmt.Sprintf("genesis transfer unsuccessful, port: %s, channel: %s: seq: %d", port, channel, seq))
 	}
+	if k.hasSeqNum(ctx, port, channel, seq) {
+		k.delSeqNum(ctx, port, channel, seq)
+		cnt := k.getNumUnackedSeqNums(ctx, port, channel)
+		cnt--
+		k.saveNumUnackedSeqNums(ctx, port, channel, cnt)
+	}
 }
 
-func (k Keeper) getLastSequenceNumber(ctx sdk.Context, port, channel string) uint64 {
-	bz := ctx.KVStore(k.storeKey).Get(seqNumKey(port, channel))
-	return sdk.BigEndianToUint64(bz)
+func (k Keeper) numUnackedSeqNums(ctx sdk.Context, port, channel string) uint64 {
+	if !success {
+		panic(fmt.Sprintf("genesis transfer unsuccessful, port: %s, channel: %s: seq: %d", port, channel, seq))
+	}
 }
 
 // genesisIsFinished returns if the genesis bridge protocol phase is finished. It is finished
