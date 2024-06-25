53d53473b7cd17c7b2f51b6ebbac89f292f58047
danwt <30197399+danwt@users.noreply.github.com>
2024-06-25 10:49:38 +0100

    do not allow ibc denoms in transfer genesis

--- x/hub-genesis/types/state.go
+++ x/hub-genesis/types/state.go
@@ -3,12 +3,17 @@ package types
 import (
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dymensionxyz/gerr-cosmos/gerrc"
+	"github.com/dymensionxyz/sdk-utils/utils/uibc"
 )
 
 func (s State) Validate() error {
 	for _, a := range s.GetGenesisAccounts() {
 		if err := a.GetAmount().Validate(); err != nil {
 			return errorsmod.Wrap(err, "amount")
+		}
+		if uibc.IsIBCDenom(a.Amount.GetDenom()) {
+			return errorsmod.Wrapf(gerrc.ErrInvalidArgument, "ibc denoms not allowed in genesis accounts: %s", a.Amount)
 		}
 		_, err := sdk.AccAddressFromBech32(a.GetAddress())
 		if err != nil {
