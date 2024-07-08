23b96dea3641bd6b69631b4284ecde7a771400e7
Daniel T <30197399+danwt@users.noreply.github.com>
2024-07-08 15:47:50 +0100

    fix(doc): adds a doc.go for hub genesis (#478)

---
+++ x/hub-genesis/doc.go
@@ -0,0 +1,4 @@
+// Package hub_genesis implements the rollapp side of the genesis bridge / genesis transfers protocol.
+// See the hub doc https://github.com/dymensionxyz/dymension/blob/8a232ee4d17c3bb74f65f882391650fd9e01d55c/x/rollapp/transfergenesis/doc.go
+// and the adr for more info https://www.notion.so/dymension/ADR-x-Genesis-Bridge-Phase-2-89769aa551b5440b9ed403a101775ce1
+package hub_genesis
