911c964711f589acfd98daffc137c57e9ee4fa24
danwt <30197399+danwt@users.noreply.github.com>
2024-08-15 16:44:29 +0100

    ready to try create

--- x/sequencers/types/msgs.go
+++ x/sequencers/types/msgs.go
@@ -6,3 +6,29 @@ var (
 	_ sdk.Msg = (*MsgCreateSequencer)(nil)
 	_ sdk.Msg = (*MsgUpdateSequencer)(nil)
 )
+
+func (m *MsgCreateSequencer) ValidateBasic() error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (m *MsgCreateSequencer) GetSigners() []sdk.AccAddress {
+	addr, err := sdk.AccAddressFromBech32(m.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{addr}
+}
+
+func (m *MsgUpdateSequencer) ValidateBasic() error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (m *MsgUpdateSequencer) GetSigners() []sdk.AccAddress {
+	addr, err := sdk.AccAddressFromBech32(m.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{addr}
+}
