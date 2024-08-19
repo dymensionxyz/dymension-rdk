02941d1a179a00283eb793c7166274ba95a3ab52
danwt <30197399+danwt@users.noreply.github.com>
2024-08-19 18:11:34 +0100

    refactor cli

--- x/sequencers/client/cli/tx.go
+++ x/sequencers/client/cli/tx.go
@@ -24,8 +24,9 @@ func GetTxCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(
-		NewCreateCmd(),
 		UnsafeImportConsensusKeyCmd(),
+		NewCreateCmd(),
+		NewUpdateCmd(),
 	)
 
 	return cmd
@@ -51,7 +52,60 @@ Operator addr should be bech32 encoded.`)
 
 			acc, err := clientCtx.AccountRetriever.GetAccount(clientCtx, clientCtx.GetFromAddress())
 			if err != nil {
-				return fmt.Errorf("get account: %w", err)
+				return fmt.Errorf("get account: make sure it has funds: %s: %w", clientCtx.GetFromAddress(), err)
+			}
+
+			var keyUID string
+			keyUID = args[0]
+			var operatorAddr string
+			operatorAddr = args[1]
+
+			txf := tx.NewFactoryCLI(clientCtx, cmd.Flags())
+
+			if _, err := txf.Keybase().Key(keyUID); err != nil {
+				return fmt.Errorf("check key is available: key name: %s: %w", keyUID, err)
+			}
+
+			msg, err := types.BuildMsgCreateSequencer(types.SigningData{
+				Account: acc,
+				ChainID: clientCtx.ChainID,
+				Signer: func(msg []byte) ([]byte, cryptotypes.PubKey, error) {
+					return txf.Keybase().Sign(keyUID, msg)
+				},
+			},
+				&types.CreateSequencerPayload{OperatorAddr: operatorAddr},
+			)
+
+			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func NewUpdateCmd() *cobra.Command {
+	short := "Update a sequencer object, to set a new reward addr etc."
+	long := strings.TrimSpace(short +
+		`Requires signature from consensus address public key. Specify consensus key in keyring uid.
+`)
+
+	cmd := &cobra.Command{
+		Use:     "update-sequencer [keyring uid] [reward addr]",
+		Example: "update-sequencer fookey ethm1lhk5cnfrhgh26w5r6qft36qerg4dclfev9nprc --from foouser",
+		Args:    cobra.ExactArgs(2),
+		Short:   short,
+		Long:    long,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			acc, err := clientCtx.AccountRetriever.GetAccount(clientCtx, clientCtx.GetFromAddress())
+			if err != nil {
+				return fmt.Errorf("get account: make sure it has funds: %s: %w", clientCtx.GetFromAddress(), err)
 			}
 
 			var keyUID string
