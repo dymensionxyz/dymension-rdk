885ffe6151a25ab6c9fbd22b2c6dff5992ce0130
shandongzhejiang <shandongzhejiang@icloud.com>
2024-08-21 11:42:48 +0800

    chore: fix some comments
    Signed-off-by: shandongzhejiang <shandongzhejiang@icloud.com>

--- x/dist/doc.go
+++ x/dist/doc.go
@@ -2,7 +2,7 @@
 Package dist defines a "wrapper" module around the Cosmos SDK's native
 x/distribution module. In other words, it provides the exact same functionality as
 the native module in that it simply embeds the native module. However, it
-overrides the AllocateTokens function so the the allocation of rewards is done
+overrides the AllocateTokens function so the allocation of rewards is done
 towards the Sequencer, the stakers, and the community pool.
 */
 package dist
