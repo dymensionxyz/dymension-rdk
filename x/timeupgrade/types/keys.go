1f0619825e32c93ba0b7d36a516e3640be4c8cb0
Faulty Tolly <@faulttolerance.net>
2024-09-17 15:40:47 +0200

    remove unused code

--- x/timeupgrade/types/keys.go
+++ x/timeupgrade/types/keys.go
@@ -1,10 +1,5 @@
 package types
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/address"
-)
-
 const (
 	// ModuleName defines the module name
 	ModuleName = "timeupgrade"
@@ -14,14 +9,3 @@ const (
 	// RouterKey defines the module's message routing key
 	RouterKey = ModuleName
 )
-
-var (
-	// Keys for store prefixes
-
-	SequencersKey = []byte{0x21} // prefix for each key to a sequencer
-)
-
-// GetSequencerKey creates the key for the sequencer with address
-func GetSequencerKey(operatorAddr sdk.ValAddress) []byte {
-	return append(SequencersKey, address.MustLengthPrefix(operatorAddr)...)
-}
