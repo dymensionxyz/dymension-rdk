855c25f5c441fbb47df0040171acd8520e54c432
Michael Tsitrin <michael@dymension.xyz>
2024-10-01 11:37:02 +0300

    added genesis info query

--- x/hub-genesis/types/query.pb.go
+++ x/hub-genesis/types/query.pb.go
@@ -195,40 +195,130 @@ func (m *QueryStateResponse) GetState() State {
 	return State{}
 }
 
+// QueryGenesisInfoRequest is the request type for the Query/GenesisInfo RPC method.
+type QueryGenesisInfoRequest struct {
+}
+
+func (m *QueryGenesisInfoRequest) Reset()         { *m = QueryGenesisInfoRequest{} }
+func (m *QueryGenesisInfoRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryGenesisInfoRequest) ProtoMessage()    {}
+func (*QueryGenesisInfoRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_361b89f8abfb35e2, []int{4}
+}
+func (m *QueryGenesisInfoRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryGenesisInfoRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryGenesisInfoRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryGenesisInfoRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryGenesisInfoRequest.Merge(m, src)
+}
+func (m *QueryGenesisInfoRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryGenesisInfoRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryGenesisInfoRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryGenesisInfoRequest proto.InternalMessageInfo
+
+// QueryGenesisInfoResponse is the response type for the Query/GenesisInfo RPC method.
+type QueryGenesisInfoResponse struct {
+	// genesis_info holds the genesis info of the rollapp
+	GenesisInfo GenesisInfo `protobuf:"bytes,1,opt,name=genesis_info,json=genesisInfo,proto3" json:"genesis_info"`
+}
+
+func (m *QueryGenesisInfoResponse) Reset()         { *m = QueryGenesisInfoResponse{} }
+func (m *QueryGenesisInfoResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryGenesisInfoResponse) ProtoMessage()    {}
+func (*QueryGenesisInfoResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_361b89f8abfb35e2, []int{5}
+}
+func (m *QueryGenesisInfoResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryGenesisInfoResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryGenesisInfoResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryGenesisInfoResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryGenesisInfoResponse.Merge(m, src)
+}
+func (m *QueryGenesisInfoResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryGenesisInfoResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryGenesisInfoResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryGenesisInfoResponse proto.InternalMessageInfo
+
+func (m *QueryGenesisInfoResponse) GetGenesisInfo() GenesisInfo {
+	if m != nil {
+		return m.GenesisInfo
+	}
+	return GenesisInfo{}
+}
+
 func init() {
 	proto.RegisterType((*QueryParamsRequest)(nil), "rollapp.hub_genesis.QueryParamsRequest")
 	proto.RegisterType((*QueryParamsResponse)(nil), "rollapp.hub_genesis.QueryParamsResponse")
 	proto.RegisterType((*QueryStateRequest)(nil), "rollapp.hub_genesis.QueryStateRequest")
 	proto.RegisterType((*QueryStateResponse)(nil), "rollapp.hub_genesis.QueryStateResponse")
+	proto.RegisterType((*QueryGenesisInfoRequest)(nil), "rollapp.hub_genesis.QueryGenesisInfoRequest")
+	proto.RegisterType((*QueryGenesisInfoResponse)(nil), "rollapp.hub_genesis.QueryGenesisInfoResponse")
 }
 
 func init() { proto.RegisterFile("hub-genesis/query.proto", fileDescriptor_361b89f8abfb35e2) }
 
 var fileDescriptor_361b89f8abfb35e2 = []byte{
-	// 361 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0xb1, 0x4e, 0x3a, 0x31,
-	0x1c, 0xc7, 0xaf, 0xe4, 0x0f, 0x43, 0xff, 0x93, 0x85, 0x44, 0x72, 0x9a, 0xd3, 0xdc, 0x20, 0x2c,
-	0x5c, 0x0d, 0x46, 0x12, 0x56, 0x66, 0x07, 0x84, 0xcd, 0xc5, 0xf4, 0xa4, 0x39, 0x2e, 0x42, 0x5b,
-	0xae, 0xbd, 0x84, 0x73, 0xf4, 0x05, 0x34, 0xe1, 0x41, 0x7c, 0x0d, 0x46, 0x12, 0x17, 0x27, 0x63,
-	0xc0, 0x07, 0x31, 0xb4, 0x25, 0x42, 0x40, 0x71, 0x83, 0xfe, 0xbe, 0xbf, 0xef, 0xe7, 0x73, 0xbd,
-	0x83, 0x87, 0xfd, 0x34, 0xac, 0x45, 0x94, 0x51, 0x19, 0x4b, 0x3c, 0x4a, 0x69, 0x92, 0x05, 0x22,
-	0xe1, 0x8a, 0xa3, 0x62, 0xc2, 0x07, 0x03, 0x22, 0x44, 0xd0, 0x4f, 0xc3, 0x5b, 0x1b, 0x70, 0x4b,
-	0x11, 0x8f, 0xb8, 0x9e, 0xe3, 0xe5, 0x2f, 0x13, 0x75, 0x8f, 0x23, 0xce, 0xa3, 0x01, 0xc5, 0x44,
-	0xc4, 0x98, 0x30, 0xc6, 0x15, 0x51, 0x31, 0x67, 0xd2, 0x4e, 0xcb, 0xeb, 0x04, 0x41, 0x12, 0x32,
-	0x5c, 0x4d, 0x36, 0xd8, 0x52, 0x11, 0x45, 0xcd, 0xc0, 0x2f, 0x41, 0x74, 0xbd, 0x54, 0x69, 0xeb,
-	0x74, 0x87, 0x8e, 0x52, 0x2a, 0x95, 0xdf, 0x86, 0xc5, 0x8d, 0x53, 0x29, 0x38, 0x93, 0x14, 0x35,
-	0x61, 0xc1, 0xb4, 0x96, 0xc1, 0x29, 0xa8, 0xfe, 0xaf, 0x1f, 0x05, 0x3b, 0xcc, 0x03, 0xb3, 0xd4,
-	0xfa, 0x37, 0x7d, 0x3f, 0x71, 0x3a, 0x76, 0xc1, 0x2f, 0xc2, 0x03, 0xdd, 0xd8, 0x5d, 0xb2, 0x57,
-	0x98, 0x2b, 0x0b, 0xb7, 0x87, 0x96, 0xd2, 0x80, 0x79, 0x6d, 0x68, 0x21, 0xee, 0x4e, 0x88, 0x5e,
-	0xb1, 0x0c, 0x13, 0xaf, 0xbf, 0xe4, 0x60, 0x5e, 0xd7, 0xa1, 0x09, 0x80, 0x05, 0x63, 0x81, 0x2a,
-	0x3b, 0xb7, 0xb7, 0x1f, 0xd9, 0xad, 0xee, 0x0f, 0x1a, 0x3f, 0xbf, 0xf1, 0xf8, 0xfa, 0x39, 0xc9,
-	0x9d, 0xa3, 0x00, 0xf7, 0xb2, 0x21, 0x65, 0x32, 0xe6, 0x6c, 0x9c, 0x3d, 0x7c, 0xff, 0xa9, 0x25,
-	0xbd, 0x7b, 0xbc, 0xfd, 0x26, 0xd0, 0x13, 0x80, 0x79, 0xad, 0x8d, 0xce, 0x7e, 0x66, 0xad, 0xdf,
-	0x8f, 0x5b, 0xd9, 0x9b, 0xb3, 0x4a, 0x97, 0x5a, 0x09, 0xa3, 0xda, 0x5f, 0x95, 0xf4, 0x8d, 0xb5,
-	0xba, 0xd3, 0xb9, 0x07, 0x66, 0x73, 0x0f, 0x7c, 0xcc, 0x3d, 0xf0, 0xbc, 0xf0, 0x9c, 0xd9, 0xc2,
-	0x73, 0xde, 0x16, 0x9e, 0x73, 0xd3, 0x8c, 0x62, 0xd5, 0x4f, 0xc3, 0xe0, 0x8e, 0x0f, 0x7f, 0xab,
-	0x1c, 0x6f, 0x94, 0xaa, 0x4c, 0x50, 0x19, 0x16, 0xf4, 0x87, 0x75, 0xf1, 0x15, 0x00, 0x00, 0xff,
-	0xff, 0x4a, 0x6a, 0xbc, 0x50, 0xef, 0x02, 0x00, 0x00,
+	// 436 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x93, 0xc1, 0xae, 0xd2, 0x40,
+	0x14, 0x86, 0x3b, 0x2a, 0x2c, 0x06, 0x37, 0x0e, 0x24, 0x60, 0x35, 0x95, 0x74, 0x21, 0x2c, 0x6c,
+	0x6b, 0x50, 0x49, 0x48, 0x5c, 0xb1, 0x31, 0x24, 0x2e, 0x10, 0x76, 0x6e, 0x48, 0x2b, 0x43, 0x69,
+	0x84, 0x99, 0xd2, 0x99, 0x26, 0xd4, 0xa5, 0x2f, 0xa0, 0x09, 0x2f, 0xe1, 0xa3, 0xb0, 0x24, 0x71,
+	0x63, 0x5c, 0xdc, 0xdc, 0xc0, 0x7d, 0x90, 0x1b, 0x66, 0x86, 0x50, 0xd2, 0x72, 0x61, 0x07, 0xe7,
+	0xfc, 0xe7, 0xff, 0xfe, 0x9c, 0x33, 0x85, 0xd5, 0x69, 0xec, 0x59, 0x3e, 0x26, 0x98, 0x05, 0xcc,
+	0x59, 0xc4, 0x38, 0x4a, 0xec, 0x30, 0xa2, 0x9c, 0xa2, 0x72, 0x44, 0x67, 0x33, 0x37, 0x0c, 0xed,
+	0x69, 0xec, 0x8d, 0x94, 0x40, 0xaf, 0xf8, 0xd4, 0xa7, 0xa2, 0xef, 0xec, 0x7f, 0x49, 0xa9, 0xfe,
+	0xd2, 0xa7, 0xd4, 0x9f, 0x61, 0xc7, 0x0d, 0x03, 0xc7, 0x25, 0x84, 0x72, 0x97, 0x07, 0x94, 0x30,
+	0xd5, 0xad, 0xa5, 0x09, 0xa1, 0x1b, 0xb9, 0xf3, 0x43, 0xe7, 0x84, 0xcd, 0xb8, 0xcb, 0xb1, 0x6c,
+	0x98, 0x15, 0x88, 0xbe, 0xec, 0xa3, 0xf4, 0x85, 0x7a, 0x80, 0x17, 0x31, 0x66, 0xdc, 0xec, 0xc3,
+	0xf2, 0x49, 0x95, 0x85, 0x94, 0x30, 0x8c, 0x3a, 0xb0, 0x28, 0x5d, 0x6b, 0xa0, 0x0e, 0x9a, 0xa5,
+	0xd6, 0x0b, 0x3b, 0x27, 0xb9, 0x2d, 0x87, 0xba, 0x4f, 0xd6, 0x37, 0xaf, 0xb4, 0x81, 0x1a, 0x30,
+	0xcb, 0xf0, 0x99, 0x70, 0x1c, 0xee, 0xd9, 0x07, 0xcc, 0x67, 0x05, 0x57, 0x45, 0x45, 0x69, 0xc3,
+	0x82, 0x48, 0xa8, 0x20, 0x7a, 0x2e, 0x44, 0x8c, 0x28, 0x86, 0x94, 0x9b, 0xcf, 0x61, 0x55, 0xb8,
+	0x7d, 0x92, 0x92, 0x1e, 0x99, 0xd0, 0x03, 0x08, 0xc3, 0x5a, 0xb6, 0xa5, 0x70, 0x3d, 0xf8, 0x54,
+	0x99, 0x8e, 0x02, 0x32, 0xa1, 0x8a, 0x5a, 0xcf, 0xa5, 0xa6, 0xe6, 0x15, 0xbb, 0xe4, 0x1f, 0x4b,
+	0xad, 0xff, 0x8f, 0x61, 0x41, 0x70, 0xd0, 0x0a, 0xc0, 0xa2, 0xdc, 0x03, 0x6a, 0xe4, 0x3a, 0x65,
+	0x97, 0xae, 0x37, 0x2f, 0x0b, 0x65, 0x64, 0xb3, 0xfd, 0xf3, 0xef, 0xdd, 0xea, 0xd1, 0x5b, 0x64,
+	0x3b, 0xe3, 0x64, 0x8e, 0x09, 0x0b, 0x28, 0x59, 0x26, 0x3f, 0x8e, 0x7f, 0xac, 0x68, 0xfc, 0xdd,
+	0xc9, 0xbe, 0x05, 0xf4, 0x0b, 0xc0, 0x82, 0x58, 0x1c, 0x7a, 0x7d, 0x9e, 0x95, 0xbe, 0x90, 0xde,
+	0xb8, 0xa8, 0x53, 0x91, 0x3e, 0x88, 0x48, 0x0e, 0xb2, 0xae, 0x8d, 0x24, 0x6e, 0x86, 0xfe, 0x00,
+	0x58, 0x4a, 0x2d, 0x15, 0xbd, 0x39, 0xcf, 0xcb, 0x9e, 0x55, 0xb7, 0xae, 0x54, 0xab, 0x8c, 0x1f,
+	0x45, 0xc6, 0x36, 0x7a, 0x7f, 0x6d, 0xc6, 0xf4, 0xbb, 0xe8, 0x0e, 0xd7, 0x5b, 0x03, 0x6c, 0xb6,
+	0x06, 0xb8, 0xdd, 0x1a, 0xe0, 0xf7, 0xce, 0xd0, 0x36, 0x3b, 0x43, 0xfb, 0xb7, 0x33, 0xb4, 0xaf,
+	0x1d, 0x3f, 0xe0, 0xd3, 0xd8, 0xb3, 0xbf, 0xd1, 0xf9, 0x43, 0xce, 0xcb, 0x13, 0x6f, 0x9e, 0x84,
+	0x98, 0x79, 0x45, 0xf1, 0x15, 0xbe, 0xbb, 0x0f, 0x00, 0x00, 0xff, 0xff, 0x82, 0x77, 0xbc, 0xb5,
+	0x1c, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -247,6 +337,8 @@ type QueryClient interface {
 	Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error)
 	// State returns the state of the genesis event.
 	State(ctx context.Context, in *QueryStateRequest, opts ...grpc.CallOption) (*QueryStateResponse, error)
+	// GenesisInfo returns the genesis info of the rollapp.
+	GenesisInfo(ctx context.Context, in *QueryGenesisInfoRequest, opts ...grpc.CallOption) (*QueryGenesisInfoResponse, error)
 }
 
 type queryClient struct {
@@ -275,12 +367,23 @@ func (c *queryClient) State(ctx context.Context, in *QueryStateRequest, opts ...
 	return out, nil
 }
 
+func (c *queryClient) GenesisInfo(ctx context.Context, in *QueryGenesisInfoRequest, opts ...grpc.CallOption) (*QueryGenesisInfoResponse, error) {
+	out := new(QueryGenesisInfoResponse)
+	err := c.cc.Invoke(ctx, "/rollapp.hub_genesis.Query/GenesisInfo", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
 	// Params returns the total set of hub-genesis parameters.
 	Params(context.Context, *QueryParamsRequest) (*QueryParamsResponse, error)
 	// State returns the state of the genesis event.
 	State(context.Context, *QueryStateRequest) (*QueryStateResponse, error)
+	// GenesisInfo returns the genesis info of the rollapp.
+	GenesisInfo(context.Context, *QueryGenesisInfoRequest) (*QueryGenesisInfoResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
@@ -292,6 +395,9 @@ func (*UnimplementedQueryServer) Params(ctx context.Context, req *QueryParamsReq
 }
 func (*UnimplementedQueryServer) State(ctx context.Context, req *QueryStateRequest) (*QueryStateResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method State not implemented")
+}
+func (*UnimplementedQueryServer) GenesisInfo(ctx context.Context, req *QueryGenesisInfoRequest) (*QueryGenesisInfoResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GenesisInfo not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
@@ -334,6 +440,24 @@ func _Query_State_Handler(srv interface{}, ctx context.Context, dec func(interfa
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_GenesisInfo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryGenesisInfoRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).GenesisInfo(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/rollapp.hub_genesis.Query/GenesisInfo",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).GenesisInfo(ctx, req.(*QueryGenesisInfoRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "rollapp.hub_genesis.Query",
 	HandlerType: (*QueryServer)(nil),
@@ -345,6 +469,10 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "State",
 			Handler:    _Query_State_Handler,
+		},
+		{
+			MethodName: "GenesisInfo",
+			Handler:    _Query_GenesisInfo_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -463,6 +591,62 @@ func (m *QueryStateResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *QueryGenesisInfoRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryGenesisInfoRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryGenesisInfoRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryGenesisInfoResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryGenesisInfoResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryGenesisInfoResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	{
+		size, err := m.GenesisInfo.MarshalToSizedBuffer(dAtA[:i])
+		if err != nil {
+			return 0, err
+		}
+		i -= size
+		i = encodeVarintQuery(dAtA, i, uint64(size))
+	}
+	i--
+	dAtA[i] = 0xa
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	offset -= sovQuery(v)
 	base := offset
@@ -510,6 +694,26 @@ func (m *QueryStateResponse) Size() (n int) {
 	var l int
 	_ = l
 	l = m.State.Size()
+	n += 1 + l + sovQuery(uint64(l))
+	return n
+}
+
+func (m *QueryGenesisInfoRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *QueryGenesisInfoResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = m.GenesisInfo.Size()
 	n += 1 + l + sovQuery(uint64(l))
 	return n
 }
@@ -762,6 +966,139 @@ func (m *QueryStateResponse) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			if err := m.State.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryGenesisInfoRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryGenesisInfoRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryGenesisInfoRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryGenesisInfoResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryGenesisInfoResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryGenesisInfoResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field GenesisInfo", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := m.GenesisInfo.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
