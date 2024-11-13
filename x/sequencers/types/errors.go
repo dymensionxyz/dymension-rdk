a8dd214d207c9de19de89b4a0f981ee14642f854
keruch <verendeev.kirill.2000@gmail.com>
2024-11-13 19:42:47 +0100

    fix: clarified whitelisted relayer logs

--- x/sequencers/types/errors.go
+++ x/sequencers/types/errors.go
@@ -5,6 +5,7 @@ import (
 )
 
 var (
-	ErrSequencerNotFound      = errorsmod.Register(ModuleName, 6, "sequencer address not found")
-	ErrHistoricalInfoNotFound = errorsmod.Register(ModuleName, 7, "historical info not found")
+	ErrSequencerNotFound           = errorsmod.Register(ModuleName, 6, "sequencer address not found")
+	ErrHistoricalInfoNotFound      = errorsmod.Register(ModuleName, 7, "historical info not found")
+	ErrWhitelistedRelayersNotFound = errorsmod.Register(ModuleName, 8, "whitelisted relayers not found")
 )
