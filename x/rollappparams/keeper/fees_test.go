478c7e60ec10c9d6ea535ae49575b1db86ace37f
keruch <verendeev.kirill.2000@gmail.com>
2024-12-04 17:56:06 +0100

    feat(rollappparams): added param for min gas price and respective ante handlers

---
+++ x/rollappparams/keeper/fees_test.go
@@ -0,0 +1,123 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+
+	testkeepers "github.com/dymensionxyz/dymension-rdk/testutil/keepers"
+	"github.com/dymensionxyz/dymension-rdk/testutil/utils"
+	"github.com/dymensionxyz/dymension-rdk/x/rollappparams/types"
+)
+
+func TestCheckFeeCoinsAgainstMinGasPrices(t *testing.T) {
+	app := utils.Setup(t, false)
+	k, ctx := testkeepers.NewTestRollappParamsKeeperFromApp(app)
+
+	tests := []struct {
+		name                  string
+		feeCoins              sdk.Coins
+		gas                   uint64
+		validatorMinGasPrices sdk.DecCoins
+		globalMinGasPrices    sdk.DecCoins
+		expectErr             bool
+	}{
+		{
+			name:                  "valid fee, global is empty, validator is empty",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(1000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{},
+			globalMinGasPrices:    sdk.DecCoins{},
+			expectErr:             false,
+		},
+		{
+			name:                  "valid fee, global is empty, validator is not empty",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(1000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 adym
+			globalMinGasPrices:    sdk.DecCoins{},
+			expectErr:             false,
+		},
+		{
+			name:                  "insufficient fee, global is empty, validator is not empty",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(500))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 adym
+			globalMinGasPrices:    sdk.DecCoins{},
+			expectErr:             true,
+		},
+		{
+			name:                  "valid fee, global is not empty, validator is empty",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(1000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{},
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 adym
+			expectErr:             false,
+		},
+		{
+			name:                  "insufficient fee, global is not empty, validator is empty",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(500))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{},
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 adym
+			expectErr:             true,
+		},
+		{
+			name:                  "valid fee, global is not empty, validator is not empty, no intersection",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(1000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("uatom", sdk.NewDecWithPrec(1, 3))}, // 0.001 uatom
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))},  // 0.001 adym
+			expectErr:             true,
+		},
+		{
+			name:                  "insufficient fee, global is greater than validator",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(1000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 uatom
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 2))}, // 0.01 adym
+			expectErr:             true,
+		},
+		{
+			name:                  "valid fee, global is greater than validator",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(10000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 uatom
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 2))}, // 0.01 adym
+			expectErr:             false,
+		},
+		{
+			name:                  "insufficient fee, validator is greater than global",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(1000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 2))}, // 0.01 uatom
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 adym
+			expectErr:             true,
+		},
+		{
+			name:                  "valid fee, validator is greater than global",
+			feeCoins:              sdk.NewCoins(sdk.NewCoin("adym", sdk.NewInt(10000))),
+			gas:                   1_000_000,
+			validatorMinGasPrices: sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 2))}, // 0.01 uatom
+			globalMinGasPrices:    sdk.DecCoins{sdk.NewDecCoinFromDec("adym", sdk.NewDecWithPrec(1, 3))}, // 0.001 adym
+			expectErr:             false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx = ctx.WithMinGasPrices(tt.validatorMinGasPrices)
+			params := types.DefaultParams()
+			params.MinGasPrices = tt.globalMinGasPrices
+			k.SetParams(ctx, params)
+
+			err := k.CheckFeeCoinsAgainstMinGasPrices(ctx, tt.feeCoins, tt.gas)
+			if tt.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
