5cc3d819013c7af5abbdd8cbc8cd4f70df60b674
zale144 <aleksandar.sukovic@gmail.com>
2024-12-08 15:37:50 +0100

    fix: only allow whitelisted relayer account to send IBC relayer messages (#614)

--- server/ante/bypass_ibc_fee_decorator.go
+++ server/ante/bypass_ibc_fee_decorator.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/cosmos/cosmos-sdk/x/group"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
-	distrkeeper "github.com/dymensionxyz/dymension-rdk/x/dist/keeper"
-	seqkeeper "github.com/dymensionxyz/dymension-rdk/x/sequencers/keeper"
 	"github.com/dymensionxyz/dymension-rdk/x/sequencers/types"
 )
 
@@ -16,11 +17,20 @@ type anteHandler interface {
 
 type BypassIBCFeeDecorator struct {
 	nextAnte anteHandler
-	dk       distrkeeper.Keeper
-	sk       seqkeeper.Keeper
+	dk       distrKeeper
+	sk       sequencerKeeper
 }
 
-func NewBypassIBCFeeDecorator(nextAnte anteHandler, dk distrkeeper.Keeper, sk seqkeeper.Keeper) BypassIBCFeeDecorator {
+type distrKeeper interface {
+	GetPreviousProposerConsAddr(ctx sdk.Context) sdk.ConsAddress
+}
+
+type sequencerKeeper interface {
+	GetSequencerByConsAddr(ctx sdk.Context, consAddr sdk.ConsAddress) (stakingtypes.Validator, bool)
+	GetWhitelistedRelayers(ctx sdk.Context, operatorAddr sdk.ValAddress) (types.WhitelistedRelayers, error)
+}
+
+func NewBypassIBCFeeDecorator(nextAnte anteHandler, dk distrKeeper, sk sequencerKeeper) BypassIBCFeeDecorator {
 	return BypassIBCFeeDecorator{
 		nextAnte: nextAnte,
 		dk:       dk,
@@ -28,40 +38,44 @@ func NewBypassIBCFeeDecorator(nextAnte anteHandler, dk distrkeeper.Keeper, sk se
 	}
 }
 
-// AnteHandle skips fee deduct and min gas price ante handlers for whitelisted relayer messages
+// AnteHandle allows IBC relayer messages and skips fee deduct and min gas price ante handlers for whitelisted relayer messages
 func (n BypassIBCFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
-	// Check if the tx is from whitelisted relayer
-	whitelisted, err := n.isWhitelistedRelayer(ctx, tx.GetMsgs())
+	msgs := tx.GetMsgs()
+	var err error
+	msgs, err = n.getAllFinalMsgs(ctx, msgs, 0)
 	if err != nil {
-		// This error is not critical; just log and fall into the default fee handling
-		ctx.Logger().With("module", "BypassIBCFeeDecorator", "err", err).
-			Error("Failed to check if the tx is from the whitelisted relayer")
+		return ctx, err
 	}
-	if whitelisted {
-		// The tx is from the whitelisted relayer, so it's eligible for the fee exemption
+
+	totalMsgs := len(msgs)
+	ibcCount := countIBCMsgs(msgs)
+
+	if ibcCount == totalMsgs {
+		// all are IBC messages
+		if err = n.isIBCWhitelistedRelayer(ctx, msgs); err != nil {
+			return ctx, err
+		}
 		return next(ctx, tx, simulate)
+	} else if ibcCount > 0 {
+		// mixed: some IBC and some non-IBC
+		return ctx, fmt.Errorf("mixed IBC and non-IBC messages in the same transaction not allowed")
 	}
 
 	// The tx if not from the whitelisted relayer; proceed with the default fee handling
 	return n.nextAnte.AnteHandle(ctx, tx, simulate, next)
 }
 
-// isWhitelistedRelayer checks if all the messages in the transaction are from whitelisted IBC relayer
-func (n BypassIBCFeeDecorator) isWhitelistedRelayer(ctx sdk.Context, msgs []sdk.Msg) (bool, error) {
-	// Check if the tx is from IBC relayer
-	if !IsIBCRelayerMsg(msgs) {
-		return false, nil
-	}
-
+// isIBCWhitelistedRelayer checks if all the messages in the transaction are from whitelisted IBC relayer
+func (n BypassIBCFeeDecorator) isIBCWhitelistedRelayer(ctx sdk.Context, msgs []sdk.Msg) error {
 	consAddr := n.dk.GetPreviousProposerConsAddr(ctx)
 	seq, ok := n.sk.GetSequencerByConsAddr(ctx, consAddr)
 	if !ok {
-		return false, fmt.Errorf("get sequencer by consensus addr: %s: %w", consAddr.String(), types.ErrSequencerNotFound)
+		return fmt.Errorf("get sequencer by consensus addr: %s: %w", consAddr.String(), types.ErrSequencerNotFound)
 	}
 	operatorAddr := seq.GetOperator()
 	wlRelayers, err := n.sk.GetWhitelistedRelayers(ctx, operatorAddr)
 	if err != nil {
-		return false, fmt.Errorf("get whitelisted relayers: sequencer address %s: %w", consAddr.String(), err)
+		return fmt.Errorf("get whitelisted relayers: sequencer address %s: %w", consAddr.String(), err)
 	}
 
 	wlRelayersMap := make(map[string]struct{}, len(msgs))
@@ -74,10 +88,64 @@ func (n BypassIBCFeeDecorator) isWhitelistedRelayer(ctx sdk.Context, msgs []sdk.
 		for _, signer := range signers {
 			_, ok := wlRelayersMap[signer.String()]
 			if !ok {
-				return false, nil
+				// if not a whitelisted relayer, we block them from sending the IBC relayer messages
+				return fmt.Errorf("signer %s is not a whitelisted relayer", signer.String())
 			}
 		}
 	}
 
-	return true, nil
+	return nil
+}
+
+const maxDepth = 6
+
+// getAllFinalMsgs recursively unpacks container messages (like MsgExec or MsgSubmitProposal) to extract all "final" (non-container) messages.
+//
+// Container messages may nest other container messages, potentially multiple levels deep.
+// This function traverses these nested structures until it reaches only final messages that cannot be further expanded.
+//
+// Parameters:
+//   - ctx:   The current context.
+//   - msgs:  The slice of sdk.Msg to unpack.
+//   - depth: The current recursion depth, with depth=0 meaning top-level messages.
+//
+// Returns:
+//   - A slice of final sdk.Msg that are not containers.
+//   - An error if the nesting exceeds maxDepth or if an error occurs while retrieving inner messages.
+//
+// If the maximum depth (maxDepth) is exceeded, it returns an error to prevent infinite recursion or overly deep nesting.
+func (n BypassIBCFeeDecorator) getAllFinalMsgs(ctx sdk.Context, msgs []sdk.Msg, depth int) ([]sdk.Msg, error) {
+	if depth >= maxDepth {
+		return nil, fmt.Errorf("found more nested msgs than permitted. Limit is : %d", maxDepth)
+	}
+
+	var final []sdk.Msg
+	for _, msg := range msgs {
+		inner, err := n.getInnerMsgs(ctx, msg, depth)
+		if err != nil {
+			return nil, err
+		}
+		final = append(final, inner...)
+	}
+	return final, nil
+}
+
+// getInnerMsgs handles nested messages and returns the final messages contained inside them.
+func (n BypassIBCFeeDecorator) getInnerMsgs(ctx sdk.Context, msg sdk.Msg, depth int) ([]sdk.Msg, error) {
+	var f func() ([]sdk.Msg, error)
+	switch m := msg.(type) {
+	case *authz.MsgExec:
+		f = m.GetMessages
+	case *group.MsgSubmitProposal:
+		f = m.GetMsgs
+	}
+	if f != nil {
+		messages, err := f()
+		if err != nil {
+			return nil, err
+		}
+		return n.getAllFinalMsgs(ctx, messages, depth+1)
+	}
+	// it's a final message
+	return []sdk.Msg{msg}, nil
 }
