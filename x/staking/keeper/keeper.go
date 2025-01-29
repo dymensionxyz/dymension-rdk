9bdc626af6d281811a28a23ad484abbbcf341832
Michael Tsitrin <michael@dymension.xyz>
2025-01-29 12:37:00 +0200

    handle undelegate erc20 tokens

--- x/staking/keeper/keeper.go
+++ x/staking/keeper/keeper.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ethereum/go-ethereum/common"
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
@@ -10,6 +11,8 @@ import (
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
+	types2 "github.com/dymensionxyz/dymension-rdk/x/staking/types"
+	erc20types "github.com/evmos/evmos/v12/x/erc20/types"
 )
 
 // Implements ValidatorSet interface
@@ -21,16 +24,18 @@ var _ types.DelegationSet = Keeper{}
 // keeper of the staking store
 type Keeper struct {
 	stakingkeeper.Keeper
+	erc20k types2.ERC20Keeper
 }
 
 // NewKeeper creates a new staking Keeper instance
 func NewKeeper(
 	cdc codec.BinaryCodec, key storetypes.StoreKey, ak types.AccountKeeper, bk types.BankKeeper,
-	ps paramtypes.Subspace,
+	erc20k types2.ERC20Keeper, ps paramtypes.Subspace,
 ) Keeper {
 	k := stakingkeeper.NewKeeper(cdc, key, ak, bk, ps)
 	return Keeper{
 		Keeper: k,
+		erc20k: erc20k,
 	}
 }
 
@@ -45,4 +50,90 @@ func (k Keeper) ApplyAndReturnValidatorSetUpdates(ctx sdk.Context) (updates []ab
 func (k *Keeper) SetHooks(sh types.StakingHooks) *Keeper {
 	k.Keeper = *k.Keeper.SetHooks(sh)
 	return k
+}
+
+func MustValAddressFromBech32(addr string) sdk.ValAddress {
+	valAddr, err := sdk.ValAddressFromBech32(addr)
+	if err != nil {
+		panic(err)
+	}
+
+	return valAddr
+}
+
+// BlockValidatorUpdates calculates the ValidatorUpdates for the current block
+// Called in each EndBlock
+// It was copied from the staking module, and modified to add erc20 conversion after unbonding
+func (k Keeper) BlockValidatorUpdates(ctx sdk.Context) {
+	_, err := k.ApplyAndReturnValidatorSetUpdates(ctx)
+	if err != nil {
+		k.Logger(ctx).Error("Failed to apply and return validator set updates", "err", err)
+		return
+	}
+
+	// unbond all mature validators from the unbonding queue
+	k.UnbondAllMatureValidators(ctx)
+
+	// Remove all mature unbonding delegations from the ubd queue.
+	matureUnbonds := k.DequeueAllMatureUBDQueue(ctx, ctx.BlockHeader().Time)
+	for _, dvPair := range matureUnbonds {
+		addr := MustValAddressFromBech32(dvPair.ValidatorAddress)
+		delegatorAddress := sdk.MustAccAddressFromBech32(dvPair.DelegatorAddress)
+
+		balances, err := k.CompleteUnbonding(ctx, delegatorAddress, addr)
+		if err != nil {
+			k.Logger(ctx).Error("Failed to complete unbonding", "err", err, "delegator", delegatorAddress, "validator", addr)
+			continue
+		}
+
+		// if coin has been registered to ERC20, convert it
+		// we continue on error, as no harm done if conversion fails
+		for _, coin := range balances {
+			if k.erc20k.IsDenomRegistered(ctx, coin.Denom) {
+				msg := erc20types.NewMsgConvertCoin(coin, common.BytesToAddress(delegatorAddress), delegatorAddress)
+				if _, err = k.erc20k.ConvertCoin(sdk.WrapSDKContext(ctx), msg); err != nil {
+					k.Logger(ctx).Error("Failed to convert coin", "err", err, "delegator", delegatorAddress, "validator", addr)
+					continue
+				}
+			}
+		}
+
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeCompleteUnbonding,
+				sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
+				sdk.NewAttribute(types.AttributeKeyValidator, dvPair.ValidatorAddress),
+				sdk.NewAttribute(types.AttributeKeyDelegator, dvPair.DelegatorAddress),
+			),
+		)
+	}
+
+	// Remove all mature redelegations from the red queue.
+	matureRedelegations := k.DequeueAllMatureRedelegationQueue(ctx, ctx.BlockHeader().Time)
+	for _, dvvTriplet := range matureRedelegations {
+		valSrcAddr := MustValAddressFromBech32(dvvTriplet.ValidatorSrcAddress)
+		valDstAddr := MustValAddressFromBech32(dvvTriplet.ValidatorDstAddress)
+		delegatorAddress := sdk.MustAccAddressFromBech32(dvvTriplet.DelegatorAddress)
+
+		balances, err := k.CompleteRedelegation(
+			ctx,
+			delegatorAddress,
+			valSrcAddr,
+			valDstAddr,
+		)
+		if err != nil {
+			k.Logger(ctx).Error("Failed to complete redelegation", "err", err, "delegator", delegatorAddress, "validatorSrc", valSrcAddr, "validatorDst", valDstAddr)
+			continue
+		}
+
+		ctx.EventManager().EmitEvent(
+			sdk.NewEvent(
+				types.EventTypeCompleteRedelegation,
+				sdk.NewAttribute(sdk.AttributeKeyAmount, balances.String()),
+				sdk.NewAttribute(types.AttributeKeyDelegator, dvvTriplet.DelegatorAddress),
+				sdk.NewAttribute(types.AttributeKeySrcValidator, dvvTriplet.ValidatorSrcAddress),
+				sdk.NewAttribute(types.AttributeKeyDstValidator, dvvTriplet.ValidatorDstAddress),
+			),
+		)
+	}
 }
