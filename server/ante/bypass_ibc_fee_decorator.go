1f25f2514fb22dc005a7a07d52ee5bc64945fe60
danwt <30197399+danwt@users.noreply.github.com>
2025-02-18 11:24:28 +0000

    fmt

--- server/ante/bypass_ibc_fee_decorator.go
+++ server/ante/bypass_ibc_fee_decorator.go
@@ -149,8 +149,8 @@ func (d BypassIBCFeeDecorator) whitelistedRelayer(ctx sdk.Context, msgs []sdk.Ms
 func isIBCNormalMsg(m sdk.Msg) bool {
 	switch m.(type) {
 	case
-			*channeltypes.MsgRecvPacket, *channeltypes.MsgAcknowledgement,
-			*channeltypes.MsgTimeout, *channeltypes.MsgTimeoutOnClose, *clienttypes.MsgUpdateClient:
+		*channeltypes.MsgRecvPacket, *channeltypes.MsgAcknowledgement,
+		*channeltypes.MsgTimeout, *channeltypes.MsgTimeoutOnClose, *clienttypes.MsgUpdateClient:
 		return true
 	}
 	return false
@@ -160,17 +160,17 @@ func isIBCLifecycleMsg(m sdk.Msg) bool {
 	switch m.(type) {
 	case
 		// Client Messages
-			*clienttypes.MsgCreateClient,
-			*clienttypes.MsgUpgradeClient, *clienttypes.MsgSubmitMisbehaviour,
+		*clienttypes.MsgCreateClient,
+		*clienttypes.MsgUpgradeClient, *clienttypes.MsgSubmitMisbehaviour,
 
 		// Connection Messages
-			*conntypes.MsgConnectionOpenInit, *conntypes.MsgConnectionOpenTry,
-			*conntypes.MsgConnectionOpenAck, *conntypes.MsgConnectionOpenConfirm,
+		*conntypes.MsgConnectionOpenInit, *conntypes.MsgConnectionOpenTry,
+		*conntypes.MsgConnectionOpenAck, *conntypes.MsgConnectionOpenConfirm,
 
 		// Channel Messages
-			*channeltypes.MsgChannelOpenInit, *channeltypes.MsgChannelOpenTry,
-			*channeltypes.MsgChannelOpenAck, *channeltypes.MsgChannelOpenConfirm,
-			*channeltypes.MsgChannelCloseInit, *channeltypes.MsgChannelCloseConfirm:
+		*channeltypes.MsgChannelOpenInit, *channeltypes.MsgChannelOpenTry,
+		*channeltypes.MsgChannelOpenAck, *channeltypes.MsgChannelOpenConfirm,
+		*channeltypes.MsgChannelCloseInit, *channeltypes.MsgChannelCloseConfirm:
 
 		return true
 	}
