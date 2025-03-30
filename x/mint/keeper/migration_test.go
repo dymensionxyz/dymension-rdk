8ba764a4424646a43a6ee846a510ae0e52a7473a
Michael Tsitrin <michael@dymension.xyz>
2025-03-30 09:59:21 +0300

    fixed migration UT

--- x/mint/keeper/migration_test.go
+++ x/mint/keeper/migration_test.go
@@ -3,10 +3,11 @@ package keeper_test
 import (
 	"testing"
 
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	testkeepers "github.com/dymensionxyz/dymension-rdk/testutil/keepers"
 	utils "github.com/dymensionxyz/dymension-rdk/testutil/utils"
 	keeper "github.com/dymensionxyz/dymension-rdk/x/mint/keeper"
-	v2 "github.com/dymensionxyz/dymension-rdk/x/mint/types/migrations/v2"
+	minttypes "github.com/dymensionxyz/dymension-rdk/x/mint/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,19 +16,20 @@ func TestMigrate1to2(t *testing.T) {
 	app := utils.Setup(t, false)
 	k, ctx := testkeepers.NewTestMintKeeperFromApp(app)
 
-	// set old style params
-	oldParams := v2.DefaultParams()
-	m := keeper.NewMigrator(*k, app.ParamsKeeper.Subspace("empty"))
-	m.SetOldParams(ctx, oldParams)
+	// raw access to the mint params subspace
+	tmpMintSubspace := paramstypes.NewSubspace(app.AppCodec(), app.LegacyAmino(), app.GetKey(paramstypes.StoreKey), app.GetTKey(paramstypes.TStoreKey), minttypes.ModuleName)
 
-	// no denom in minter
-	minter := k.GetMinter(ctx)
-	minter.MintDenom = ""
-	k.SetMinter(ctx, minter)
+	// migrator sets old v1 keyTable on this subspace
+	m := keeper.NewMigrator(*k, tmpMintSubspace)
+	tmpMintSubspace.Set(ctx, []byte("MintDenom"), "denom")           // simulate v1 denom in params
+	tmpMintSubspace.Set(ctx, []byte("MintEpochIdentifier"), "epoch") // assertion that the subpsace is actually changed in the x/mint keeper
+
+	// make sure the above changes reflected in the keeper
+	params := k.GetParams(ctx)
+	require.Equal(t, "epoch", params.MintEpochIdentifier)
 
 	err := m.Migrate1to2(ctx)
 	require.NoError(t, err)
 
-	minter = k.GetMinter(ctx)
-	require.Equal(t, oldParams.MintDenom, minter.MintDenom)
+	require.Equal(t, "denom", k.GetMinter(ctx).MintDenom)
 }
