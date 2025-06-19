0df5043f46109b452dd42a99c1e5c3836404fe9d
jinjingroad <jinjingroad@sina.com>
2025-06-19 15:08:07 +0800

    chore: make function comment match function name
    Signed-off-by: jinjingroad <jinjingroad@sina.com>

--- x/tokenfactory/keeper/createdenom.go
+++ x/tokenfactory/keeper/createdenom.go
@@ -9,7 +9,7 @@ import (
 	"github.com/dymensionxyz/dymension-rdk/x/tokenfactory/types"
 )
 
-// ConvertToBaseToken converts a fee amount in a whitelisted fee token to the base fee token amount
+// CreateDenom converts a fee amount in a whitelisted fee token to the base fee token amount
 func (k Keeper) CreateDenom(ctx sdk.Context, creatorAddr string, subdenom string) (newTokenDenom string, err error) {
 	denom, err := k.validateCreateDenom(ctx, creatorAddr, subdenom)
 	if err != nil {
