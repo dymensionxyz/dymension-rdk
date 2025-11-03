6133cc8f2dfcc75626c5ea1bdcc4bbc508a75d70
Michael Tsitrin <michael@dymension.xyz>
2025-11-03 17:17:54 +0200

    updated proto

--- x/hub/types/state.go
+++ x/hub/types/state.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
@@ -11,7 +12,18 @@ func (s *State) Validate() error {
 		if _, ok := denomMap[d.Base]; ok {
 			return errorsmod.Wrapf(gerrc.ErrInvalidArgument, "duplicate denom in registered denoms: %s", d)
 		}
+		if err := sdk.ValidateDenom(d.Base); err != nil {
+			return errorsmod.Wrapf(err, "invalid denom: %s", d.Base)
+		}
 		denomMap[d.Base] = struct{}{}
 	}
+
+	// Validate decimal conversion pair if it exists
+	if s.Hub.DecimalConversionPair != nil {
+		if err := s.Hub.DecimalConversionPair.Validate(); err != nil {
+			return errorsmod.Wrapf(err, "invalid decimal conversion pair")
+		}
+	}
+
 	return nil
 }
